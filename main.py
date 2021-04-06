e3ccf68e1e7b0c65f9e6b636b2e3b976784339f1
Mikhail Zoreev <mzoreev@gmail.com>
2021-04-06 23:51:52 +0300

    path explorer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import os
 from argparse import ArgumentParser
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 
 
 def __get_argument_parser() -> ArgumentParser:
@@ -19,9 +19,37 @@ def main(arguments):
     os.makedirs('shared', exist_ok=True)
     app = Flask(__name__, static_url_path='')
 
+    @app.route('/download/')
+    def show_root():
+        full_path = './shared/'
+        if not os.path.exists(full_path):
+            return
+        files = os.listdir(full_path)
+        links = []
+        for file in files:
+            links.append({
+                'name': file,
+                'url': '/download/' + '/' + file
+            })
+        print(links)
+        return render_template('index.html', links=links)
+
     @app.route('/download/<path:path>')
     def download_file(path):
-        return send_from_directory('shared', path)
+        full_path = './shared/' + path
+        if not os.path.exists(full_path):
+            return
+        if os.path.isfile(full_path):
+            return send_from_directory('shared', path)
+        files = os.listdir(full_path)
+        links = []
+        for file in files:
+            links.append({
+                'name': file,
+                'url': '/download/' + path + '/' + file
+            })
+        print(links)
+        return render_template('index.html', links=links)
 
     app.run(host='0.0.0.0', port=arguments.port)
 
